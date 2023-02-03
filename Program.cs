b4f26ae0f655c0d19fd33cc7f43cd62101bb49c2
HaruGrimgar <rewcom05@gmail.com>
2023-02-04 01:13:03 +0300

    example02 and gitignore

--- Program.cs
+++ Program.cs
@@ -1,1 +1,4 @@
-﻿Console.WriteLine("2 Hello, World!");
+﻿Console.WhiteLine("Введите своё имя, пожалуйста");
+string username = Console.ReadLine();
+Console.WhiteLine("Привет, ");
+Console.WriteLine(username);
