a08a0da40cffddc75def0bbdb294f342e9467fae
HaruGrimgar <rewcom05@gmail.com>
2023-02-04 08:35:43 +0300

    Деление

--- 3_частное_чисел/Program.cs
+++ 3_частное_чисел/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double numberA = 12;
+Console.WriteLine(numberA);
+double numberB = 5;
+Console.WriteLine(numberA);
+Console.WriteLine(numberA / numberB); 
+// при  12 на 5 получаем 2 () -> чтобы решить эту проблему надо поменять формат чисел
